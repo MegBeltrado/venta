db13414993ee31056858f7a17ba4983838d19801
Fanny <145309171+FannyE23@users.noreply.github.com>
2023-09-27 20:22:54 -0600

    Update _Inserts.sql

--- Sistema venta/Base Datos/002_Inserts.sql
+++ Sistema venta/Base Datos/002_Inserts.sql
@@ -1,19 +1,13 @@
 
---________________________________ INSERTAR ROLES ________________________________
 insert into rol(descripcion,esActivo) values
 ('Administrador',1),
 ('Empleado',1),
 ('Supervisor',1)
 
-
---________________________________ INSERTAR USUARIO ________________________________
 SELECT * FROM Usuario
 --clave : 123
 insert into Usuario(nombre,correo,telefono,idRol,urlFoto,nombreFoto,clave,esActivo) values
 ('codigo estudiante','codigo@example.com','909090',1,'','','a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3',1)
-
---________________________________ RECURSOS DE FIREBASE_STORAGE Y CORREO ________________________________
---(AQUI DEBES INCLUIR TUS PROPIAS CLAVES Y CRENDENCIALES)
 
 insert into Configuracion(recurso,propiedad,valor) values
 ('FireBase_Storage','email',''),
@@ -32,16 +26,12 @@ insert into Configuracion(recurso,propiedad,valor) values
 ('Servicio_Correo','puerto','587')
 
 
---________________________________ INSERTAR NEGOCIO ________________________________
+-
 select * from Negocio
 
 insert into Negocio(idNegocio,urlLogo,nombreLogo,numeroDocumento,nombre,correo,direccion,telefono,porcentajeImpuesto,simboloMoneda)
 values(1,'','','','','','','',0,'')
-
-
---________________________________ INSERTAR CATEGORIAS ________________________________
 SELECT * FROM Categoria
-
 INSERT INTO Categoria(descripcion,esActivo) values
 ('Computadoras',1),
 ('Laptops',1),
@@ -50,8 +40,6 @@ INSERT INTO Categoria(descripcion,esActivo) values
 ('Microfonos',1)
 
 
---________________________________ INSERTAR TIPO DOCUMENTO VENTA ________________________________
-
 select * from TipoDocumentoVenta
 
 insert into TipoDocumentoVenta(descripcion,esActivo) values
@@ -59,14 +47,12 @@ insert into TipoDocumentoVenta(descripcion,esActivo) values
 ('Factura',1)
 
 
---________________________________ INSERTAR NUMERO CORRELATIVO ________________________________
+
 select * from NumeroCorrelativo
 --000001
 insert into NumeroCorrelativo(ultimoNumero,cantidadDigitos,gestion,fechaActualizacion) values
 (0,6,'venta',getdate())
 
-
---________________________________ INSERTAR MENUS ________________________________
 select * from Menu
 
 --*menu padre
@@ -74,7 +60,7 @@ insert into Menu(descripcion,icono,controlador,paginaAccion,esActivo) values
 ('DashBoard','fas fa-fw fa-tachometer-alt','DashBoard','Index',1)
 
 insert into Menu(descripcion,icono,esActivo) values
-('Administración','fas fa-fw fa-cog',1),
+('AdministraciÃ³n','fas fa-fw fa-cog',1),
 ('Inventario','fas fa-fw fa-clipboard-list',1),
 ('Ventas','fas fa-fw fa-tags',1),
 ('Reportes','fas fa-fw fa-chart-area',1)
@@ -104,7 +90,6 @@ insert into Menu(descripcion,idMenuPadre, controlador,paginaAccion,esActivo) val
 UPDATE Menu SET idMenuPadre = idMenu where idMenuPadre is null
 
 
---________________________________ INSERTAR ROL MENU ________________________________
 select * from Menu
 select * from RolMenu
 SELECT * FROM ROL
